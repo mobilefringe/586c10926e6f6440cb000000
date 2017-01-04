f7963155f8a46f4183ca778c9ab9cd38c1980163
caitlin@mobilefringe.com @ CodeCloud <caitlin@mobilefringe.com @ CodeCloud>
2017-01-04 11:47:01 -0500

    Quinte Mall 1.0  /application.js

--- application.js
+++ application.js
@@ -641,7 +641,17 @@ function renderPostDetails(container, template, collection){
     });
     $(container).html(item_rendered.join(''));
 }
-
+function renderFashion(container, template, collection){
+    var item_list = [];
+    var item_rendered = [];
+    var template_html = $(template).html();
+    Mustache.parse(template_html); 
+    $.each( collection , function( key, val ) {
+        var repo_rendered = Mustache.render(template_html,val);
+        item_rendered.push(repo_rendered);
+    });
+    $(container).html(item_rendered.join(''));
+}
 function show_png_pin(trigger, map){
     $(trigger).bind("change", function(e) {
         e.preventDefault()
