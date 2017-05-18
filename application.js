f51ff576b12100c9faee9d50223e964ecfc3d5e7
caitlin@mobilefringe.com @ CodeCloud <caitlin@mobilefringe.com @ CodeCloud>
2017-05-18 13:31:02 -0400

    Quinte Mall 1.0  /application.js

--- application.js
+++ application.js
@@ -344,11 +344,12 @@ function renderEvents(container, template, collection){
         else {
             val.store_name = "Tecumseh Mall";
             val.image_url = val.event_image_url_abs;
-        }
-        
-        if(val.event_image_url_abs.indexOf('missing.png') < 0){
+            if(val.event_image_url_abs.indexOf('missing.png') < 0){
             val.image_url = "//codecloud.cdn.speedyrails.net/sites/586c10926e6f6440cb000000/image/jpeg/1495128383000/quinte_default.jpg";
         }
+        }
+        
+        
         // else{
         //     if(val.image_url.indexOf('missing.png') < 0){
         //         val.logo = val.image_url;
