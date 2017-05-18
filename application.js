1d074942b2e7a790449c06d66d61b4df581e9ad6
caitlin@mobilefringe.com @ CodeCloud <caitlin@mobilefringe.com @ CodeCloud>
2017-05-18 13:47:45 -0400

    Quinte Mall 1.0  /application.js

--- application.js
+++ application.js
@@ -344,6 +344,9 @@ function renderEvents(container, template, collection){
         else {
             val.store_name = "Quinte Mall";
             val.image_url = val.event_image_url_abs;
+            if(val.image_url.indexOf('missing.png') < -1){
+                val.image_url = "//codecloud.cdn.speedyrails.net/sites/586c10926e6f6440cb000000/image/jpeg/1495128383000/quinte_default.jpg";
+            }
         }
         
         
